6cdb7f805c6d00f3c5f64e6798432d8921b71f92
callaway12 <taewankim0925@likelion.org>
2021-10-28 20:43:17 +0900

    asdf

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from user import views
 urlpatterns = [
-    path('/singup', views.signup, name="sign_up"),
+    path('singup/', views.signup, name="sign_up"),
+    path('home/', views.home, name="home"),
 ]
